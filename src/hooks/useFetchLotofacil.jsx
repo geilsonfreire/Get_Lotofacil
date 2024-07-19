8d00691f38b62b04a14b474aa13c96d067cb6457
geilsonfreire <geilsonfreireleite@gmail.com>
2024-07-18 22:18:12 -0300

    style: Atualizar estilos da página de análise e tabela de resultados e aplicando regras de estilo condicional

--- src/hooks/useFetchLotofacil.jsx
+++ src/hooks/useFetchLotofacil.jsx
@@ -1,5 +1,8 @@
+// Import Bibliotecas
 import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
+
+// Import Services
 import APIService from '../services/APIServices';
 
 const useFetchLotofacil = () => {
@@ -8,38 +11,38 @@ const useFetchLotofacil = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        let toastId = null;
+        let toastId = null; // Variável para armazenar o id do toast
 
         const fetchResults = async () => {
             try {
-                const response = await APIService.getLotofacil();
-                setResults(response.data);
-                setLoading(false);
+                const response = await APIService.getLotofacil(); // Chamada da API
+                setResults(response.data); // Atualiza o estado com os dados da API
+                setLoading(false); // Desativa o loading
 
                 if (!toastId) {
                     toastId = toast.info("Dados atualizados.", {
                         autoClose: 3000,
-                        onClose: () => setLoading(false),
+                        onClose: () => setLoading(false), // Desativa o loading
                     });
                 } else {
-                    setLoading(false);
+                    setLoading(false); // Desativa o loading
                 }
             } catch (error) {
-                console.error("Erro ao carregar dados do concurso:", error);
-                setError(error);
-                toast.error("Erro ao carregar dados do concurso.");
-                setLoading(false);
+                console.error("Erro ao carregar dados do concurso:", error); // Exibe o erro no console
+                setError(error); // Atualiza o estado com o erro
+                toast.error("Erro ao carregar dados do concurso."); // Exibe o erro em um toast
+                setLoading(false); // Desativa o loading
             }
         };
 
-        fetchResults();
+        fetchResults(); // Chama a função fetchResults
 
         return () => {
-            if (toastId) {
-                toast.dismiss(toastId);
+            if (toastId) { // Verifica se o toastId é diferente de null
+                toast.dismiss(toastId); // Fecha o toast
             }
         };
-    }, []);
+    }, []); 
 
     return { results, loading, error };
 };
--- src/pages/analise.jsx
+++ src/pages/analise.jsx
@@ -1,5 +1,5 @@
 // Import Bibliotecas
-import React, { } from 'react';
+import React from 'react';
 
 // Import Hooks
 import useFetchLotofacil from '../hooks/useFetchLotofacil';
@@ -23,57 +23,60 @@ const Analise = () => {
         }, 0);
     };
 
-    // Chama a função para contar as ocorrências de 1 a 25
-    const N1 = countOccurrencesOfNumber(1);
-    const N2 = countOccurrencesOfNumber(2);
-    const N3 = countOccurrencesOfNumber(3);
-    const N4 = countOccurrencesOfNumber(4);
-    const N5 = countOccurrencesOfNumber(5);
-    const N6 = countOccurrencesOfNumber(6);
-    const N7 = countOccurrencesOfNumber(7);
-    const N8 = countOccurrencesOfNumber(8);
-    const N9 = countOccurrencesOfNumber(9);
-    const N10 = countOccurrencesOfNumber(10);
-    const N11 = countOccurrencesOfNumber(11);
-    const N12 = countOccurrencesOfNumber(12);
-    const N13 = countOccurrencesOfNumber(13);
-    const N14 = countOccurrencesOfNumber(14);
-    const N15 = countOccurrencesOfNumber(15);
-    const N16 = countOccurrencesOfNumber(16);
-    const N17 = countOccurrencesOfNumber(17);
-    const N18 = countOccurrencesOfNumber(18);
-    const N19 = countOccurrencesOfNumber(19);
-    const N20 = countOccurrencesOfNumber(20);
-    const N21 = countOccurrencesOfNumber(21);
-    const N22 = countOccurrencesOfNumber(22);
-    const N23 = countOccurrencesOfNumber(23);
-    const N24 = countOccurrencesOfNumber(24);
-    const N25 = countOccurrencesOfNumber(25);
+    // Armazenar as ocorrências de 1 a 25 em um array
+    const occurrences = Array.from({ length: 25 }, (_, i) => countOccurrencesOfNumber(i + 1));
+
+    // Encontrar os valores máximo e mínimo
+    const maxOccurrences = Math.max(...occurrences);
+    const minOccurrences = Math.min(...occurrences);
+
+    // Função para determinar a classe de estilo com base no valor
+    const getCellStyle = (value) => {
+        if (value === maxOccurrences) {
+            return {
+                backgroundColor: 'green', // Fundo verde para o maior valor
+                color: 'white',            // Texto branco  
+            }; // Maior valor
+        } else if (value === minOccurrences) {
+            return {
+                backgroundColor: 'red', // Fundo verde para o maior valor
+                color: 'white',            // Texto branco
+            }; // Menor valor
+        } else {
+            return {}; // Valor neutro
+        }
+    };
 
     return (
         <section className='analise'>
             <h2><i><BsGraphUpArrow /></i> Análise Estatística</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th><span>DEZ/</span>-01</th>
-                        <th><span>DEZ/</span>-02</th>
-                        <th><span>DEZ/</span>-03</th>
-                        <th><span>DEZ/</span>-04</th>
-                        <th><span>DEZ/</span>-05</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr >
-                        <td>{N1}</td>
-                        <td>{N2}</td>
-                        <td>{N3}</td>
-                        <td>{N4}</td>
-                        <td>{N5}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="soma-dez-sorteada">
+                <h3>Analise da Dez Quente / Fria</h3>
+                {Array.from({ length: 5 }, (_, rowIndex) => (
+                    <table key={rowIndex}>
+                        <thead>
+                            <tr>
+                                {Array.from({ length: 5 }, (_, colIndex) => (
+                                    <th key={colIndex}><span>DEZ/</span>{rowIndex * 5 + colIndex + 1}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {Array.from({ length: 5 }, (_, colIndex) => {
+                                    const numberIndex = rowIndex * 5 + colIndex;
+                                    return (
+                                        <td key={colIndex} style={getCellStyle(occurrences[numberIndex])}>
+                                            {occurrences[numberIndex]}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        </tbody>
+                    </table>
+                ))}
+            </div>
         </section>
     );
 };
