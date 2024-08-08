b78b0a1c86c18bae537e9f11dc3c1c832677c00e
geilsonfreire <geilsonfreireleite@gmail.com>
2024-08-08 20:12:36 -0300

    fio excluido o a pasta BAck End

--- Back_End/src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('lotofacil', 'root', 'Gkrb945965#', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: console.log, // Habilita o logging de consultas SQL
-});
-
-export default sequelize;
